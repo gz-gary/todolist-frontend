311ce23c9a3693830856ab3a4dac26b32274c441
gryz <jinlizhong@smail.nju.edu.cn>
2025-05-23 20:24:39 +0800

    Implement: make the content of about page richer

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,64 @@
+import { blogLink, githubLink, sourceCodeLink } from '@/lib/constants'
+import CodeIcon from '@mui/icons-material/Code'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import WebIcon from '@mui/icons-material/Web'
+import { Box, Button, Link, Stack, Typography } from '@mui/material'
+
 export default function About() {
   return (
-    <>
-      <h1>About</h1>
-      <p>Something about me.</p>
-    </>
+    <Box 
+      sx={{ p: 5 }}
+    >
+      <Box>
+        <Typography variant="h3" sx={{ marginBottom: 2 }}>
+          About this app
+        </Typography>
+        <Typography>
+          Todolist app, an application that help you manage your to-do list.
+        </Typography>
+        <Typography>
+          Todolist应用，一个帮助您管理您的待办事项的应用。
+        </Typography>
+      </Box>
+      <Stack 
+        spacing={2}
+        sx={{ m: 2 }}
+      >
+        {[
+          {
+            text: 'Check the source code of this page 🤗:',
+            icon: (<CodeIcon/>),
+            buttonText: 'Code',
+            route: sourceCodeLink
+          },
+          {
+            text: 'Follow me on github 😊:',
+            icon: (<GitHubIcon/>),
+            buttonText: 'Github',
+            route: githubLink
+          },
+          {
+            text: 'Visit my blog 🤓:',
+            icon: (<WebIcon/>),
+            buttonText: 'Blog',
+            route: blogLink
+          },
+        ].map(({text, icon, buttonText, route}, index) => (
+          <Box key={index}>
+            <Typography>{text}</Typography>
+            <Button
+              startIcon={icon}
+              href={route}
+              LinkComponent={Link}
+              variant="outlined"
+              sx={{ textTransform: 'none' }}
+            >
+              {buttonText}
+            </Button>
+          </Box>
+        ))}
+
+      </Stack>
+    </Box>
   )
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,4 @@
 export const appTitle = 'Todolist App'
+export const githubLink = 'https://github.com/gz-gary'
+export const sourceCodeLink = 'https://github.com/gz-gary/todolist-frontend'
+export const blogLink = 'https://zjlwiki.top'
