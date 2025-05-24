e9d142fcf39f8541bdc6504776fa2ea783bed774
gryz <jinlizhong@smail.nju.edu.cn>
2025-05-24 09:45:45 +0800

    Implement: add a tooltip for the '+' button

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,11 +1,12 @@
-import { Checkbox, List, ListItem, Typography } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+import { Box, Card, CardActions, CardContent, Checkbox, Fab, List, ListItem, Tooltip, Typography } from '@mui/material'
 
 type TodolistItem = {
   title: string,
   finished: boolean
 }
 
-export default function Todolist(finshed: boolean) {
+export default function Todolist(finished: boolean) {
   const testFinishedList: TodolistItem[] = [
     {title: 'Research report', finished: true},
   ]
@@ -17,17 +18,46 @@ export default function Todolist(finshed: boolean) {
   ]
 
   return (
-    <List>
-      {
-        (finshed ? testFinishedList : testNotFinishedList).map(
-          ({title, finished}, index) => (
-            <ListItem key={index}>
-              <Typography>{title}</Typography>
-              <Checkbox defaultChecked={finished}/>
-            </ListItem>
+    <Box sx={{ flexGrow: 1, position: 'relative' }}>
+      <List>
+        {
+          (finished ? testFinishedList : testNotFinishedList).map(
+            ({title, finished}, index) => (
+              <ListItem key={index}>
+                <Card sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    width: '100%'
+                  }}
+                >
+                  <CardContent>
+                    <Typography>{title}</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Checkbox defaultChecked={finished}/>
+                  </CardActions>
+                </Card>
+              </ListItem>
+            )
           )
-        )
-      }
-    </List>
+        }
+      </List>
+      {
+      !finished && (
+        <Fab 
+          size="medium"
+          color="primary"
+          sx={{
+            position: 'absolute',
+            bottom: 16,
+            right: 16,
+          }}
+        >
+          <Tooltip title="Add a new to-do">
+            <AddIcon/>
+          </Tooltip>
+        </Fab>
+      )}
+    </Box>
   )
 }
